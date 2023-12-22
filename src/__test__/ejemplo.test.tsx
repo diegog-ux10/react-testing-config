978f43a68bb6033f4060c723539556771f2a8b6d
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-12-22 14:22:24 -0400

    paso #03 completado
    1. archivo mock creado
    2. mapper agregado a la configuración de jest en package.json
    3. archivo ejemplo de prueba modificado

--- src/__test__/ejemplo.test.tsx
+++ src/__test__/ejemplo.test.tsx
@@ -1,10 +1,6 @@
 import { render } from "@testing-library/react";
-// 👀 importa el componente App a este aquí ❗
+import App from "../App";
 
 test("loads and displays greeting", () => {
-  render(
-    <div>
-      <h1>Hola, Soy una prueba</h1>
-    </div> /* 👈 Cambia todo lo que se esta dentro de render por el componente App 😬*/
-  );
+  render(<App />);
 });
