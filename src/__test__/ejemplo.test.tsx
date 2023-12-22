f4477631b60c6136bb9293037578fc582d1a0b90
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-12-22 15:50:52 -0400

    paso #04 completado
    prueba ejemplo estructurada con patrón triple a

--- src/__test__/ejemplo.test.tsx
+++ src/__test__/ejemplo.test.tsx
@@ -1,6 +1,13 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("loads and displays greeting", () => {
+test("loads and displays greeting", async () => {
+  // ✅ ARRANGE
   render(<App />);
+
+  // ✅ ACT
+  await screen.findByRole("heading");
+
+  // ✅ ASSERT
+  expect(screen.getAllByText("Vite + React")).not.toBeNull();
 });
